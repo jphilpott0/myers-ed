5a88506b202a708d913285d0cb1647ac27f249e7
Josh Philpott <73914347+jphilpott0@users.noreply.github.com>
2025-11-19 03:50:42 +0000

    Add benchmark

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,6 +1,8 @@
 #![feature(const_index, const_trait_impl)]
 
-use criterion::*;
+use core::hint::black_box;
+
+use criterion::{Criterion, criterion_group, criterion_main};
 
 use myers_ed::prelude::*;
 
@@ -8,36 +10,85 @@ use myers_ed::prelude::*;
 const BRCA2_C50: &[u8] = &include_bytes!("brca2_c50.txt")[..512];
 const BRCA2_C50_MUT: &[u8] = &include_bytes!("brca2_c50_mut.txt")[..512];
 
-pub fn bench_myers_ed_single_avx512_12x12(c: &mut Criterion) {
-    c.bench_function("bench_myers_ed_single_avx512_12x12", |b| {
-        b.iter(|| black_box(myers_ed_single_avx512(b"GATCAATGACTG", b"GATCAATAACTG")))
-    });
+mod scalar {
+    use super::*;
+
+    pub fn bench_myers_ed_single_scalar_12x12(c: &mut Criterion) {
+        c.bench_function("bench_myers_ed_single_scalar_12x12", |b| {
+            b.iter(|| black_box(myers_ed_single_scalar(b"GATCAATGACTG", b"GATCAATAACTG")))
+        });
+    }
+
+    pub fn bench_myers_ed_single_scalar_with_peq_12x12(c: &mut Criterion) {
+        let peq = SingleWordPeq::from_bytes(b"GATCAATGACTG");
+
+        c.bench_function("bench_myers_ed_single_scalar_with_peq_12x12", |b| {
+            b.iter(|| black_box(myers_ed_single_scalar_with_peq(&peq, b"GATCAATAACTG")))
+        });
+    }
+
+    pub fn bench_myers_ed_single_scalar_64x64(c: &mut Criterion) {
+        c.bench_function("bench_myers_ed_single_scalar_64x64", |b| {
+            b.iter(|| {
+                black_box(myers_ed_single_scalar(
+                    &BRCA2_C50[..64],
+                    &BRCA2_C50_MUT[..64],
+                ))
+            })
+        });
+    }
+
+    pub fn bench_myers_ed_single_scalar_with_peq_64x64(c: &mut Criterion) {
+        let peq = SingleWordPeq::from_bytes(&BRCA2_C50[..64]);
+
+        c.bench_function("bench_myers_ed_single_scalar_with_peq_64x64", |b| {
+            b.iter(|| black_box(myers_ed_single_scalar_with_peq(&peq, &BRCA2_C50_MUT[..64])))
+        });
+    }
 }
 
-pub fn bench_myers_ed_single_avx512_with_peq_12x12(c: &mut Criterion) {
-    let peq = SingleWordPeq::from_bytes(b"GATCAATGACTG");
+use scalar::*;
 
-    c.bench_function("bench_myers_ed_single_avx512_12x12", |b| {
-        b.iter(|| black_box(myers_ed_single_avx512_with_peq(&peq, b"GATCAATAACTG")))
-    });
+mod avx512 {
+    use super::*;
+
+    pub fn bench_myers_ed_single_avx512_12x12(c: &mut Criterion) {
+        c.bench_function("bench_myers_ed_single_avx512_12x12", |b| {
+            b.iter(|| black_box(myers_ed_single_avx512(b"GATCAATGACTG", b"GATCAATAACTG")))
+        });
+    }
+
+    pub fn bench_myers_ed_single_avx512_with_peq_12x12(c: &mut Criterion) {
+        let peq = SingleWordPeq::from_bytes(b"GATCAATGACTG");
+
+        c.bench_function("bench_myers_ed_single_avx512_with_peq_12x12", |b| {
+            b.iter(|| black_box(myers_ed_single_avx512_with_peq(&peq, b"GATCAATAACTG")))
+        });
+    }
+
+    pub fn bench_myers_ed_single_avx512_512x512(c: &mut Criterion) {
+        c.bench_function("bench_myers_ed_single_avx512_512x512", |b| {
+            b.iter(|| black_box(myers_ed_single_avx512(BRCA2_C50, BRCA2_C50_MUT)))
+        });
+    }
+
+    pub fn bench_myers_ed_single_avx512_with_peq_512x512(c: &mut Criterion) {
+        let peq = SingleWordPeq::from_bytes(BRCA2_C50);
+
+        c.bench_function("bench_myers_ed_single_avx512_with_peq_512x512", |b| {
+            b.iter(|| black_box(myers_ed_single_avx512_with_peq(&peq, BRCA2_C50_MUT)))
+        });
+    }
 }
 
-pub fn bench_myers_ed_single_avx512_512x512(c: &mut Criterion) {
-    c.bench_function("bench_myers_ed_single_avx512_512x512", |b| {
-        b.iter(|| black_box(myers_ed_single_avx512(BRCA2_C50, BRCA2_C50_MUT)))
-    });
-}
-
-pub fn bench_myers_ed_single_avx512_with_peq_512x512(c: &mut Criterion) {
-    let peq = SingleWordPeq::from_bytes(BRCA2_C50);
-
-    c.bench_function("bench_myers_ed_single_avx512_512x512", |b| {
-        b.iter(|| black_box(myers_ed_single_avx512_with_peq(&peq, BRCA2_C50_MUT)))
-    });
-}
+use avx512::*;
 
 criterion_group!(
     benches,
+    bench_myers_ed_single_scalar_12x12,
+    bench_myers_ed_single_scalar_with_peq_12x12,
+    bench_myers_ed_single_scalar_64x64,
+    bench_myers_ed_single_scalar_with_peq_64x64,
     bench_myers_ed_single_avx512_12x12,
     bench_myers_ed_single_avx512_with_peq_12x12,
     bench_myers_ed_single_avx512_512x512,
